aac3a722f76a9c2aae5da1fe25460c7167ec61d8
shlomzion cohn <shlomzioncohn@gmail.com>
2026-07-09 17:42:29 +0300

    implement GameSnapshot

--- KFChessProject/src/models/GameSnapshot.java
+++ KFChessProject/src/models/GameSnapshot.java
@@ -1,4 +1,27 @@
 package models;
 
+
+/**
+ * GameSnapshot מייצג תמונת מצב קפואה ובלתי ניתנת לשינוי (Immutable) של המשחק.
+ */
 public class GameSnapshot {
+
+    private final Board board;
+
+    private final GameState gameState;
+
+
+    public GameSnapshot(Board board, GameState gameState) {
+        this.board = board;
+        this.gameState = gameState;
+    }
+
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
 }
