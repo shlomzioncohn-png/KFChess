7403e8fcfcee3a836dac72cf741f886fd3dcb094
shlomzion cohn <shlomzioncohn@gmail.com>
2026-07-12 09:08:48 +0300

    write BoardMapper class

--- KFChessProject/src/input/BoardMapper.java
+++ KFChessProject/src/input/BoardMapper.java
@@ -1,4 +1,23 @@
 package input;
 
+import models.Position;
+
 public class BoardMapper {
+    public static final int CELL_SIZE = 100;
+
+    public static final int BOARD_SIZE = 8;
+
+    /**
+     * ממיר קואורדינטות פיקסל (x, y) למיקום לוגי על הלוח (Row, Col).
+     */
+    public Position mapPixelToPosition(int x, int y) {
+        int col = x / CELL_SIZE;
+        int row = y / CELL_SIZE;
+
+        if (col >= 0 && col < BOARD_SIZE && row >= 0 && row < BOARD_SIZE) {
+            return new Position(row, col);
+        }
+
+        return null;
+    }
 }
