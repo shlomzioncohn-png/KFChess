4ca086874f8b16163e18b8989293ebe19172ab76
shlomzion cohn <shlomzioncohn@gmail.com>
2026-07-12 09:57:04 +0300

    create BoardNavigator and write RookRule

--- KFChessProject/src/rules/RookRule.java
+++ KFChessProject/src/rules/RookRule.java
@@ -1,4 +1,19 @@
 package rules;
 
-public class RookRule {
+import models.Board;
+import models.Position;
+
+public class RookRule implements PieceRule {
+
+    @Override
+    public boolean isValidMove(Board board, Position from, Position to) {
+        int deltaRow = Math.abs(to.getRow() - from.getRow());
+        int deltaCol = Math.abs(to.getCol() - from.getCol());
+
+        // תנועה ישרה בלבד
+        boolean isStraight = (deltaRow == 0 && deltaCol > 0) || (deltaCol == 0 && deltaRow > 0);
+        if (!isStraight) return false;
+
+        return BoardNavigator.isPathClear(board, from, to);
+    }
 }
