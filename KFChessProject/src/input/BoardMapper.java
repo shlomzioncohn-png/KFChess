e2a556773abe94fabc0441dc3e7244314b4c4795
shlomzion cohn <shlomzioncohn@gmail.com>
2026-07-12 09:17:26 +0300

    fix mapPixelToPosition func

--- KFChessProject/src/input/BoardMapper.java
+++ KFChessProject/src/input/BoardMapper.java
@@ -1,21 +1,23 @@
 package input;
 
+import models.Board;
 import models.Position;
 
 public class BoardMapper {
     public static final int CELL_SIZE = 100;
 
-    public static final int BOARD_SIZE = 8;
 
     /**
      * ממיר קואורדינטות פיקסל (x, y) למיקום לוגי על הלוח (Row, Col).
      */
-    public Position mapPixelToPosition(int x, int y) {
+    public static Position mapPixelToPosition(int x, int y, Board board) {
         int col = x / CELL_SIZE;
         int row = y / CELL_SIZE;
 
-        if (col >= 0 && col < BOARD_SIZE && row >= 0 && row < BOARD_SIZE) {
-            return new Position(row, col);
+        Position potentialPosition = new Position(row, col);
+
+        if (board != null && board.isValidPosition(potentialPosition)) {
+            return potentialPosition;
         }
 
         return null;
